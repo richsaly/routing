62a8f9ef0c252ac86483b8046a5e82d371bb1f43
MADUKA  C.  SALVATION <salvationmaduka1@gmail.com>
2024-06-25 00:19:10 +0100

    redo

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {  HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter  as Router, Routes, Route } from 'react-router-dom';
 import Layout from './Components/Layout';
 import Home from './Components/Home';
 import About from './Components/About';
